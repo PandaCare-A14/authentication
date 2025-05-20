bb25a401bb81cef374b7f2b48b64053e23e39364
Thorbert Anson Shi <tobyas2005139@gmail.com>
2025-05-20 10:53:46 +0700

    chore: migrated to rs256 to allow usage of oauth2-resource-server library on backend server

--- src/services/jwt/signing/mod.rs
+++ src/services/jwt/signing/mod.rs
@@ -2,6 +2,7 @@ use crate::errors::jwt::JWTCreationError;
 
 pub mod es256;
 pub mod hs256;
+pub mod rs256;
 
 pub trait TokenSigner {
     fn sign(&self, claims: impl serde::Serialize) -> Result<String, JWTCreationError>;
